4c5e58932a112776d9e26af090e3873e4ed7dc9d
AntonAksyonov <AntonAksyonovForLevelUp@yandex.ru>
2025-01-14 09:09:20 +0300

    Revert "homework7"
    This reverts commit 4efd7f4ccbb00ef3d2589b6f7eb28ef6c03030f7.

--- src/java/homework7/MultiGeneralizedRunner.java
+++
@@ -1,9 +0,0 @@
-package homework7;
-
-public class MultiGeneralizedRunner {
-    public static void main(String[] args) {
-        Pet pet = new Pet("Шарик", 12, false);
-        MultiGeneralized<String, Pet, Integer> multiGeneralized = new MultiGeneralized<>("Some string", pet, 2322);
-        multiGeneralized.getFieldsClassName();
-    }
-}
