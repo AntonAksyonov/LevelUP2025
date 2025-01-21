e38fca43f9dcddb4bca24150165e93810ae0a6dc
AntonAksyonov <AntonAksyonovForLevelUp@yandex.ru>
2025-01-21 09:21:48 +0300

    homework8

---
+++ src/java/homework8/Employee.java
@@ -0,0 +1,35 @@
+package homework8;
+
+public class Employee implements Comparable<Employee> {
+    private String fio;
+    private String serviceNumber;
+    private int workExperience;
+
+    public Employee(String fio, String serviceNumber, int workExperience) {
+        this.fio = fio;
+        this.serviceNumber = serviceNumber;
+        this.workExperience = workExperience;
+    }
+
+    public int getWorkExperience() {
+        return workExperience;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fio='" + fio + '\'' +
+                ", serviceNumber='" + serviceNumber + '\'' +
+                ", workExperience=" + workExperience +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee employee) {
+        return this.fio.compareTo(employee.fio);
+    }
+}
