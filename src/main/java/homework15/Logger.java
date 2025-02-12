74c865960fa3354aa6cf0995a53a0053e9104ccb
AntonAksyonov <AntonAksyonovForLevelUp@yandex.ru>
2025-02-12 15:35:53 +0300

    edits after review

--- src/main/java/homework15/Logger.java
+++ src/main/java/homework15/Logger.java
@@ -1,22 +1,20 @@
 package homework15;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-
 import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Random;
 
 @RequiredArgsConstructor
-public class Logger extends Thread implements Runnable {
+public class Logger extends Thread {
     public static final String SRC_RESOURCES_LOG = "src/main/resources/homework15.log";
 
     private final LoggingStatus loggingLevel;
     private final String message;
 
     @Override
-    @SneakyThrows
-    public void run(){
+    public void run() {
         try {
             while (!isInterrupted()) {
                 int millisSleep = new Random().nextInt(5000);
@@ -25,8 +23,10 @@ public class Logger extends Thread implements Runnable {
                     fileWriter.write("%s %s %s %s%s".formatted(LocalDateTime.now().toString(), loggingLevel.name(), Thread.currentThread().threadId(), message, "\n"));
                 }
             }
-        } catch (InterruptedException e){
-
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            System.out.println("У потока %s при работе с файлом %s возникла ошибка %s".formatted(Thread.currentThread().threadId(), SRC_RESOURCES_LOG, e.getMessage()));
         }
     }
 }
